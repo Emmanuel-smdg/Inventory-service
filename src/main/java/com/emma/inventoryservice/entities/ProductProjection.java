9c154cdf38eef134aba734dc4f490abc62c84481
Emmanuel Samandoulougou <samabdoulougoue@gmail.com>
2023-02-10 14:11:45 +0000

    Updating app properties to register in eureka server

---
+++ src/main/java/com/emma/inventoryservice/entities/ProductProjection.java
@@ -0,0 +1,10 @@
+package com.emma.inventoryservice.entities;
+
+import org.springframework.data.rest.core.config.Projection;
+
+@Projection(name = "fullProduct", types = Product.class)
+public interface ProductProjection extends Projection {
+    public Long getId();
+    public String getName();
+    public String getPrice();
+}
